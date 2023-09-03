10094948d8d1e104ab0239474292e21e268441f6
hyeleenotharry <dlgpgl1029@gmail.com>
2023-09-03 14:35:00 +0900

    first

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "my_user"
+
+    email = models.CharField(max_length=256, default='')
